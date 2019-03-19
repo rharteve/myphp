7d8dccda3730e2bd67ae57a5f298741c1f6625d7
Robert Hartevelt <Robert.Hartevelt@planet.nl>
2019-03-19 10:33:06 +0100

    datum toegevoegd

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
 
-print("Hello World - Greetings from RoHa\n");
+function datum() {
+      date_default_timezone_set('UTC');
+      echo date('l');
+
+     $datum=date("l d-m-y"); 
+     return  $datum;
+
+}
+
+
+// MAIN
+$mydatum=datum();
+     print("Hello World - Greetings from RoHa op $mydatum \n");
+
 ?>
