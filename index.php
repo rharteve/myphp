f146837dd507707da7f3436c537f91f56bfb5ba9
Robert Hartevelt <Robert.Hartevelt@planet.nl>
2019-03-19 13:12:46 +0100

    perf up

--- index.php
+++ index.php
@@ -9,10 +9,26 @@ function datum() {
 
 }
 
+function get_cpu(){
+     $load = sys_getloadavg();
+     return $load[0];
+}
+
 
 // MAIN
+$wachttijd=6;
+$teltot=200;
+$powerme=100000000;
 $mydatum=datum();
-     print("Hello World - Greetings from RoHa op $mydatum \n");
+     printf("Wacht op sleeptijd %d .....", $wachttijd);
+     sleep($wachttijd);
+     print("klaar \n");
+     for ( $i=0; $i < $teltot; $i++){
+     //        $mydatum=datum();
+         for($x=0; $x< $powerme ; $x++) {
+         } // next x
 
-     sleep(10);
+        $mycpu=get_cpu();
+        printf("%d - CPU LOAD AVAGE = %.2f %% \n", $i, $mycpu);
+    } // next i
 ?>
